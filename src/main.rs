b37767a6ba22c0dcce99a5e1507544f91ed0d716
Greg Depoire--Ferrer <greg.depoire@gmail.com>
2022-01-23 01:03:43 +0100

    Switch to mupdf
    mupdf appears to be much faster and is well known and maintained.

--- src/main.rs
+++ src/main.rs
@@ -1,51 +1,76 @@
+use std::convert::TryFrom;
 use std::str;
 
-use pdf::build::{CatalogBuilder, PageBuilder};
-use pdf::content::{Op, TextDrawAdjusted};
-use pdf::file::File;
-use pdf::primitive::Primitive;
+use mupdf::pdf::PdfDocument;
+use mupdf::pdf::PdfObject;
+use mupdf::pdf::PdfWriteOptions;
 
-fn read_exercise_number_from_op(op: &Op) -> Option<i32> {
-    let text = match op {
-        Op::TextDrawAdjusted { array } => array,
-        _ => return None,
-    };
-    if text.len() < 5 {
-        return None;
-    }
-    match &text[0] {
-        TextDrawAdjusted::Text(s) if s.as_ref() == b"EXER" => {},
-        _ => return None,
-    };
-    match &text[2] {
-        TextDrawAdjusted::Text(s) if s.as_ref() == b"CICE" => {},
-        _ => return None,
-    };
-    match &text[4] {
-        TextDrawAdjusted::Text(s) => s.to_string().ok()?.parse().ok(),
-        _ => None,
+enum FilterResult {
+    KeepKid,
+    RemoveKid,
+    SubtractCount(i32),
+}
+
+fn filter_page_tree_helper<F: FnMut(PdfObject) -> bool>(mut node: PdfObject, f: &mut F) -> FilterResult {
+    let ty = node.get_dict("Type").unwrap().unwrap();
+    let ty_str = ty.as_name().unwrap();
+    match ty_str {
+        b"Page" => if f(node) { FilterResult::KeepKid } else { FilterResult::RemoveKid },
+        b"Pages" => {
+            let mut subtract_count = 0;
+            if let Some(mut kids) = node.get_dict("Kids").unwrap() {
+                let mut kids_len = i32::try_from(kids.len().unwrap()).unwrap();
+                let mut i: i32 = 0;
+                while i < kids_len {
+                    let kid = kids.get_array(i).unwrap().unwrap();
+                    match filter_page_tree_helper(kid, f) {
+                        FilterResult::KeepKid => {},
+                        FilterResult::RemoveKid => {
+                            kids.array_delete(i).unwrap();
+                            kids_len -= 1;
+                            subtract_count += 1;
+                            continue;
+                        },
+                        FilterResult::SubtractCount(n) => subtract_count += n,
+                    }
+                    i += 1;
+                }
+                if subtract_count > 0 {
+                    let count = node.get_dict("Count").unwrap().unwrap()
+                        .as_int().unwrap();
+                    node.dict_put("Count", PdfObject::new_int(count - subtract_count).unwrap()).unwrap();
+                }
+            }
+            FilterResult::SubtractCount(subtract_count)
+        },
+        _ => panic!("invalid type in page tree"),
     }
 }
 
+fn filter_page_tree<F: FnMut(PdfObject) -> bool>(root: PdfObject, mut f: F) {
+    filter_page_tree_helper(root, &mut f);
+}
+
 fn main() {
-    let mut file = File::open("exercises.pdf").expect("failed to load PDF");
+    let doc = PdfDocument::open("exercises.pdf").expect("failed to load PDF");
+    let catalog_id = doc.catalog().unwrap();
+    let mut catalog = catalog_id.resolve().unwrap().unwrap();
+    let tree_id = catalog.get_dict("Pages").unwrap().unwrap();
+    let tree = tree_id.resolve().unwrap().unwrap();
     let mut current_exercise_number = -1;
-    let mut pages = vec![];
-    for page in file.pages() {
-        let page = page.expect("failed to decode page");
-        if let Some(contents) = &page.contents {
-            let ops = contents.operations(&file).unwrap();
-            match ops.iter().find_map(read_exercise_number_from_op) {
-                Some(val) => current_exercise_number = val,
-                None => {},
-            }
+    filter_page_tree(tree, |page: PdfObject| {
+        let contents = page.get_dict("Contents").unwrap().unwrap()
+            .read_stream().unwrap();
+        let contents_str = str::from_utf8(&contents).unwrap();
+        let marker = "(EXER)31(CICE)";
+        if let Some(i) = contents_str.find(marker) {
+            let lparen = (i + marker.len()) + contents_str[i + marker.len()..].find('(').unwrap();
+            let rparen = (lparen + 1) + contents_str[(lparen + 1)..].find(')').unwrap();
+            current_exercise_number = contents_str[(lparen + 1)..rparen].parse().unwrap();
         }
-        if current_exercise_number == 105 {
-            pages.push(PageBuilder::from_page(&page).unwrap());
-        }
-    }
-    let catalog = CatalogBuilder::from_pages(pages)
-        .build(&mut file).unwrap();
-    file.update_catalog(catalog).expect("failed to update catalog");
-    file.save_to("exercises-extracted.pdf").expect("failed to save PDF");
+        // Remove the page unless it's the good exercise.
+        current_exercise_number == 105
+    });
+    catalog.dict_delete("Outlines").unwrap();
+    doc.save("exercises-extracted.pdf").expect("failed to save PDF");
 }
